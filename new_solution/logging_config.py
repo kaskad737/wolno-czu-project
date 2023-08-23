1c0f005e4094bfe4704a95968d4ba229c1315ca8
Apti Archakov <Archakov.Apti@email.cz>
2023-08-23 17:02:33 +0200

    Add changer to new solutions. Add gitignore

---
+++ new_solution/logging_config.py
@@ -0,0 +1,23 @@
+dict_config = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'base': {
+            'format': '%(asctime)s.%(msecs)03d | %(name)s | %(levelname)s | %(lineno)-4d | %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'base'
+        },
+    },
+    'loggers': {
+        'wolnoLogger': {
+            'level': f'DEBUG',
+            'handlers': ['console']
+        }
+    },
+}
--- new_solution/main.py
+++ new_solution/main.py
@@ -1,10 +1,69 @@
 import requests
 from bs4 import BeautifulSoup
+import psycopg2
+import logging
+import logging.config
+from logging_config import dict_config
+from itertools import takewhile
+
+logging.config.dictConfig(dict_config)
+wolno_logger: logging.Logger = logging.getLogger('wolnoLogger')
+
+
 
 URL = 'http://192.168.80.14/apcka2'
 
 response = requests.get(URL)
 soup = BeautifulSoup(response.text, 'html.parser')
 
+zones = []
+wifi_zones = {}
+
 for link in soup.find_all('a'):
-    print(link.text)
+    if (('pef' in link.get('href')) or ('cems' in link.get('href'))) and ('out' not in link.get(
+                'href')):
+        if link['href']:
+            zone_name = link['href']
+            zone_response = requests.get(f'{URL}/{zone_name}')
+
+            ############################################3
+
+            list_test = zone_response.text.split('\n')
+            test_length = len(zone_response.text.split('\n'))
+            # good point to write test:
+            # ['cems1-1-167', '39 days', '515', 'Site_5G:', 'eduroam', 'CZU-guest', 'CZU-staff', 'Site_2.4G:', 'eduroam', 'CZU-guest', 'CZU-staff', 'Pocet_lidi_5G:', '2', '0', '0', 'Pocet_lidi_2.4G:', '0', '0', '0', 'data z controlleru: ', 'vytvoreni souboru pro dane ap: 23.08.23-16:31:24', '']
+            # change quantity of users OR change quantity of 2.4G OR 5G networks 
+            # example: ['cems1-1-167', '39 days', '515', 'Site_5G:', 'eduroam', 'CZU-guest', 'CZU-staff', 'Site_2.4G:', 'eduroam', 'CZU-guest', 'CZU-staff', 'Pocet_lidi_5G:', '2', '0', 'Pocet_lidi_2.4G:', '0', '0', 'data z controlleru: ', 'vytvoreni souboru pro dane ap: 23.08.23-16:31:24', '']
+            # example: ['cems1-1-167', '39 days', '515', 'Site_5G:', 'eduroam', 'CZU-guest', 'CZU-staff', 'Site_2.4G:', 'eduroam', 'CZU-guest', 'CZU-staff', 'Pocet_lidi_5G:', '2', '2', '3', 'Pocet_lidi_2.4G:', '0', '0', 'data z controlleru: ', 'vytvoreni souboru pro dane ap: 23.08.23-16:31:24', '']
+            # list_test = ['cems1-1-167', '39 days', '515', 'Site_5G:', 'eduroam', 'CZU-guest', 'CZU-staff', 'Site_2.4G:', 'eduroam', 'CZU-guest', 'CZU-staff', 'Pocet_lidi_5G:', '2', '2', '3', 'Pocet_lidi_2.4G:', '0', '0', 'data z controlleru: ', 'vytvoreni souboru pro dane ap: 23.08.23-16:31:24', '']
+            # test_length = len(list_test)
+            print(list_test)
+            if 'Pocet_lidi_5G:' in list_test:
+                g5_index = list_test.index('Pocet_lidi_5G:')
+                g5_result = list(takewhile(lambda x: x.isdigit(), list_test[g5_index + 1:]))
+
+                print(g5_result)
+
+            if 'Pocet_lidi_2.4G:' in list_test:
+                g2_index = list_test.index('Pocet_lidi_2.4G:')
+                g2_result = list(takewhile(lambda x: x.isdigit(), list_test[g2_index + 1:]))
+
+                print(g2_result)
+
+            break
+    
+            # wifi_zones[zone_name] = {'2.4G': g2, '5G': g5}
+
+
+
+
+# for name in zones:
+#         if name:
+#             counter_ap += 1
+#         response = http.request('GET', f'http://192.168.80.14/apcka2/{name}')
+#         html = response.data.decode('utf8')
+#         networks_counts = list(chain(x.split('\n')[1:-1] for x in html.split(':')[1:5]))
+#         g2, g5 = tuple(dict(zip(networks_counts[x], networks_counts[y])) for x, y in [
+#             (1, 3),
+#             (0, 2),
+#         ])
