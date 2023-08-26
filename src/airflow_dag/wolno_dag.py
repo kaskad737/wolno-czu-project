252594f0a0292311b47991d604452150baa0f2d2
Apti Archakov <Archakov.Apti@email.cz>
2023-08-26 20:54:17 +0200

    add changes to wolno dag

--- src/airflow_dag/wolno_dag.py
+++ src/airflow_dag/wolno_dag.py
@@ -3,30 +3,30 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.models import Variable
 
+#####################################################################################
+# Variables in Airflow
+
 DB_HOST = Variable.get('DB_HOST')
 DB_NAME = Variable.get('DB_NAME')
 DB_USER = Variable.get('DB_USER')
 DB_PASSWORD = Variable.get('DB_PASSWORD')
 
 
-
 #####################################################################################
 # DAG
-
 
 wolno_dag = DAG(
     'Wolno',
     start_date=datetime.datetime(2023, 8, 26),
-    schedule_interval='*/5 * * * *',
+    schedule_interval='0 12 * * *',
     catchup = False
-
 )
 
 #####################################################################################
 # Tasks
 
 wolno_data_download_task = BashOperator(
-    task_id='Wolno data downloader task',
+    task_id='Wolno_data_downloader_task',
     bash_command=f'/opt/airflow/envs/wolno-env/bin/python /opt/airflow/scripts/wolno-czu-project/src/main.py -h {DB_HOST} -n {DB_NAME} -u {DB_USER} -p {DB_PASSWORD}',
     retries=3,
     retry_delay=datetime.timedelta(seconds=180),
